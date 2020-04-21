a7532aac7dd366b39691a4f5d0f25a2d46ff6dd6
Luca <luca.kaufmann@futurice.com>
2020-04-21 17:55:51 +0300

    Initial Commit

---
+++ Popcorn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Popcorn
+//
+//  Created by Luca Kaufmann on 21.4.2020.
+//  Copyright © 2020 mqtthings. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
